04969d930f3bb1787be72df535d00ad35b93f72a
Wei Wen <weiwen.web@gmail.com>
2017-10-29 23:28:34 -0400

    tiny

--- tutorials/image/cifar10/cifar10.py
+++ tutorials/image/cifar10/cifar10.py
@@ -72,7 +72,6 @@ MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
 NUM_EPOCHS_PER_DECAY = 350.0      # Epochs after which learning rate decays.
 LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
 INITIAL_LEARNING_RATE = 0.1       # Initial learning rate.
-#INITIAL_LEARNING_RATE = 0.001       # Initial learning rate.
 
 # If a model is trained with multiple GPUs, prefix all Op names with tower_name
 # to differentiate the operations. Note that this prefix is removed from the
@@ -359,7 +358,6 @@ def train(total_loss, global_step):
   # Compute gradients.
   with tf.control_dependencies([loss_averages_op]):
     opt = tf.train.GradientDescentOptimizer(lr)
-#    opt = tf.train.AdamOptimizer(INITIAL_LEARNING_RATE)
     grads = opt.compute_gradients(total_loss)
 
   # Apply gradients.
